2f10f040942f6a098519899b6aa0bc5dd6289233
Aosgos <segunad1997@gmail.com>
2022-11-16 15:06:31 -0800

    Help Files

--- exits.c
+++ exits.c
@@ -7,28 +7,27 @@
  *@n: the amount of characters to be copied
  *Return: the concatenated string
  */
-
 char *_strncpy(char *dest, char *src, int n)
+{
+	int i, j;
+	char *s = dest;
 
-{
-int i, j;
-char *s = dest;
-i = 0;
-while (src[i] != '\0' && i < n - 1)
-{
-dest[i] = src[i];
-i++;
-}
-if (i < n)
-{
-j = i;
-while (j < n)
-{
-dest[j] = '\0';
-j++;
-}
-}
-return (s);
+	i = 0;
+	while (src[i] != '\0' && i < n - 1)
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	if (i < n)
+	{
+		j = i;
+		while (j < n)
+		{
+			dest[j] = '\0';
+			j++;
+		}
+	}
+	return (s);
 }
 
 /**
@@ -38,25 +37,24 @@ return (s);
  *@n: the amount of bytes to be maximally used
  *Return: the concatenated string
  */
-
 char *_strncat(char *dest, char *src, int n)
+{
+	int i, j;
+	char *s = dest;
 
-{
-int i, j;
-char *s = dest;
-i = 0;
-j = 0;
-while (dest[i] != '\0')
-i++;
-while (src[j] != '\0' && j < n)
-{
-dest[i] = src[j];
-i++;
-j++;
-}
-if (j < n)
-dest[i] = '\0';
-return (s);
+	i = 0;
+	j = 0;
+	while (dest[i] != '\0')
+		i++;
+	while (src[j] != '\0' && j < n)
+	{
+		dest[i] = src[j];
+		i++;
+		j++;
+	}
+	if (j < n)
+		dest[i] = '\0';
+	return (s);
 }
 
 /**
@@ -65,13 +63,12 @@ return (s);
  *@c: the character to look for
  *Return: (s) a pointer to the memory area s
  */
-
 char *_strchr(char *s, char c)
-
 {
-do {
-if (*s == c)
-return (s);
-} while (*s++ != '\0');
-return (NULL);
+	do {
+		if (*s == c)
+			return (s);
+	} while (*s++ != '\0');
+
+	return (NULL);
 }
